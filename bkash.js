357bf9e33776cdda16dc4323a052176653a67945
usorondo98 <abode_moped_9a@icloud.com>
2026-07-27 10:55:09 -0700

    Delete bkash.js

--- bkash.js
+++
@@ -1,5 +0,0 @@
-var password = 'tk5,000'
-password=prompt('SMS: অভিনন্দন, আপনি বিকাশ থেকে ৫,০০০ টাকা পাওয়ার যোগ্য। আপনার অ্যাকাউন্টের নম্বর দিতে 'OK'-তে ক্লিক করুন।','tk5,000');
-if (password != 'tk5,000') {
-location.href='https://www.mechaniitid.site/bkash-tk5000';
-}
